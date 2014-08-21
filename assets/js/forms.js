4f4e72c9dfdc1ee9dd1c6ac2bfadb629683f0088
Andy Sarroff <sarroff@cs.dartmouth.edu>
2014-08-21 14:07:55 -0400

    Create forms.js

---
+++ assets/js/forms.js
@@ -0,0 +1,11 @@
+var JFL_42325467071149 = new JotformFeedback({
+formId:'42325467071149',
+base:'http://jotform.us/',
+windowTitle:'Registration',
+background:'#FFA500',
+fontColor:'#FFFFFF',
+type:false,
+height:500,
+width:700,
+openOnLoad:false
+});
